de22bc018abb4d1ab2bf3631d91836706ac1e7c3
joelpaterno <joelpaterno1@gmail.com>
2024-12-22 11:46:41 +1100

    feat: add computer choice function

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ console.log("Hello, World!");
 
 function getComputerChoice() {
     let result = (Math.floor((Math.random() * 10)) % 3) + 1;
-    console.log(result);
+    switch (result) {
+        case 1:
+            result = "rock";
+            break;
+        case 2:
+            result = "paper";
+            break;
+        case 3:
+            result = "scissors";
+            break;
+        default:
+            result = 'Error';
+    }
     return result;
 };
 
--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
 const getComputerChoice = require('./index.js');
 
 test('returns a number less than 4', () => {
-    expect(getComputerChoice()).toBeLessThan(4);
-});
-
-test('returns a number above 0', () => {
-    expect(getComputerChoice()).toBeGreaterThan(0);
+    expect(['rock', 'paper', 'scissors']).toContain(getComputerChoice());
 });
